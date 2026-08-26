42a7fcf9e614c961473a2e56cd88d4a9b849f19f
JYOTHISH T M <jothishtm2002@gmail.com>
2026-08-26 23:30:37 +0530

    refactor: update user service interface

--- src/services/interface/IUserService.ts
+++ src/services/interface/IUserService.ts
@@ -1,9 +1,23 @@
 
+import { IUser } from "../../models/User";
+import { TicketDocument } from "../../models/Ticket";
 
 export interface IUserService {
-  getTicketsByUserId(userId: string, page: number, limit: number): Promise<Response | null>;
+  // getTicketsByUserId(userId: string, page: number, limit: number): Promise<Response | null>;
   cancelTicketAndRefund(ticketId: string, userId: string): Promise<{ refundAmount: number }>;
   fetchLayoutAndEvent(layoutId: string):Promise<object>
-  getUserById(userId: string):Promise<object>
+  // getUserById(userId: string):Promise<object>
 
+   getUserById(userId: string): Promise<IUser | null>;
+
+  getTicketsByUserId(
+    userId: string,
+    page: number,
+    limit: number
+  ): Promise<{
+    tickets: TicketDocument[];
+    currentPage: number;
+    totalPages: number;
+    totalItems: number;
+  }>;
 }
